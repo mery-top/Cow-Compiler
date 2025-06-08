0f1c9662f6c33327765ac8ff8ab07166b185c9f9
Meerthika <meerthika@example.com>
2025-06-08 16:28:03 +0000

    [Fix] main directory

--- src/main.c
+++ src/main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "./front-end/lexer/lexer.h"
-#include "./front-end/ast/ast.h"
-#include "./front-end/parser/parser.h"
-#include "./front-end/token/token.h"
-#include "./back-end/evaluator/eval.h"
+#include "../front-end/lexer/lexer.h"
+#include "../front-end/ast/ast.h"
+#include "../front-end/parser/parser.h"
+#include "../front-end/token/token.h"
+#include "../back-end/evaluator/eval.h"
 
 
 int main(int argc, char *argv[]){
