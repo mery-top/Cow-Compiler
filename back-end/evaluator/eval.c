46a85e64e9f491cce50ebb55cb44b2aecc8174be
Meerthika <meerthika@example.com>
2025-06-08 15:39:16 +0000

    Add generate_code function in eval.c

---
+++ back-end/evaluator/eval.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "eval.h"
+
+
+void generate_code(ASTNode *node){
+    int result = 0;
+    if (node->type == AST_VAR_DECL){
+        node= node->left;
+    }
+
+    if(node->left == AST_BINARY){
+        int left = node->left->value;
+        int right = node->right->value;
+
+        result = (node->op == '+') ? (left+right) : (left-right);
+    }else if(node->left == AST_NUMBER){
+        result =  node->value;
+    }
+
+    printf("The result is:%d\n", result);
+}
