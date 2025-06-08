4816599987c7e1e47b71e1df520d374f14a8b35b
Meerthika <meerthika@example.com>
2025-06-08 13:26:31 +0000

    Add ast.h

---
+++ front-end/ast/ast.h
@@ -0,0 +1,30 @@
+#ifndef AST_H
+#define AST_H
+
+typedef enum{
+    AST_NUMBER,
+    AST_BINARY,
+    AST_VAR_DECL,
+    AST_VAR,
+    AST_ASSIGN
+} ASTNodeType;
+
+typedef struct{
+    ASTNodeType type;
+    int value;
+    char name[64];
+    struct ASTNode *left;
+    struct ASTNode *right;
+    char op;
+} ASTNode;
+
+
+ASTNode *create_num_node(int value);
+ASTNode* create_var_node(char *name);
+ASTNode *create_var_declare_node(char *name, int value);
+ASTNode *create_binary_node(char op, ASTNode *left, ASTNode *right);
+ASTNode *create_var_assign_node(char *name, int value);
+void free_ast(ASTNode *node);
+
+#endif
+
