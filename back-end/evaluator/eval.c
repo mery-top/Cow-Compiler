bea75f9a1183507f3fa124dd8d49acdea7de5aad
Meerthika <meerthika@example.com>
2025-06-10 12:59:19 +0000

    [Fix] Binary operations

--- back-end/evaluator/eval.c
+++ back-end/evaluator/eval.c
@@ -8,14 +8,19 @@ void generate_code(ASTNode *node){
         node= node->left;
     }
 
-    if(node->left->type == AST_BINARY){
-        int left = node->left->value;
-        int right = node->right->value;
+    int left = node->left->value;
+    int right = node->right->value;
 
-        result = (node->op == '+') ? (left+right) : (left-right);
-    }else if(node->left->type == AST_NUMBER){
-        result =  node->value;
-    }
+    result = (node->op == '+') ? (left+right) : (left-right);
+
+    // if(node->left->type == AST_BINARY){
+    //     int left = node->left->value;
+    //     int right = node->right->value;
+
+    //     result = (node->op == '+') ? (left+right) : (left-right);
+    // }else if(node->left->type == AST_NUMBER){
+    //     result =  node->value;
+    // }
 
     printf("The result is:%d\n", result);
 }
